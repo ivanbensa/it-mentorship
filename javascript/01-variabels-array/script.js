cdfca47345430db695402390d19d4924ef87c897
ivanbensa <ivanbensa@proton.me>
2025-10-02 23:50:25 +0200

    Update script.js

--- javascript/01-variabels-array/script.js
+++ javascript/01-variabels-array/script.js
@@ -1,1 +1,10 @@
+let name = "Ivan";
+let age = 35;
 
+console.log("Hello, my name is " + name + " and I am " + age + " years old.");
+
+///////////////////////////////////////////////////////////////////////////////////
+
+let components = ["computer", "keyboard", "mouse"];
+
+console.log(components);
