2517fe9ae4242fd72d6bafa8d1b79daefc75918f
ivanbensa <ivanbensa@proton.me>
2025-10-03 10:25:58 +0200

    Create script.js

---
+++ javascript/02-array-object/script.js
@@ -0,0 +1,29 @@
+let shop = {
+  name: "Game Shop",
+  products: ["Hitman", "Max Payne", "GTA III"],
+  openingHours: "08:00",
+  closingHours: "16:00"
+};
+
+console.log(shop);
+
+///////////////////////////////////////////////////////////////////////
+
+let names = [];
+names.push("Mary", "Peter", "John");
+
+console.log(names);
+
+///////////////////////////////////////////////////////////////////////
+
+let components = ["computer", "monitor", "keyboard", "mouse"];
+components.length = 0;
+
+console.log(components);
+
+////////////////////////////////////////////////////////////////////////
+
+let books = ["The Shining", "Misery", "It"];
+books.splice(0, 0, "The Bridge on the Drina");
+
+console.log(books);
