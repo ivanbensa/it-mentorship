7554d07ce22f9777f893a333efa3a9a60b81ed76
ivanbensa <ivanbensa@proton.me>
2025-10-03 13:02:34 +0200

    Create script.js

---
+++ javascript/03-if-else/script.js
@@ -0,0 +1,12 @@
+let name = "admin";
+let password = "secretPassword";
+
+if (name == "admin" && password == "secretPassword") {
+    console.log("Welcome back");
+} else if(name == "toma" && password == "secretPassword") {
+    console.log("Welcome Presidente");
+} else {
+    console.log("Wrong username or password");
+};
+
+//////////////////////////////////////////////////////////////////////////////////
