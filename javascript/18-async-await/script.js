f8a8cf2abcf558c338683d2c8ac9cd979522f70b
ivanbensa <ivanbensa@proton.me>
2026-09-08 01:06:07 +0200

    Update movie search exercise

--- javascript/18-async-await/script.js
+++ javascript/18-async-await/script.js
@@ -3,34 +3,47 @@ let apiKey = "da3ddb98";
 
 let searchInput = document.getElementById("movieSearchInput");
 let moviesHolder = document.getElementById("moviesHolder");
+let movieSearchForm = document.getElementById("movieSearchForm");
 
-document.getElementById("movieSearchForm").addEventListener("submit", async function (event) {
+movieSearchForm.addEventListener("submit", async function (event) {
     event.preventDefault();
 
     let search = searchInput.value;
     let response = await fetch(apiUrl + "?apikey=" + apiKey + "&s=" + search);
     let data = await response.json();
 
+
+
+    if (data.Response === "False") {
+        alert("Movie not found. Please try again.");
+        searchInput.value = "";
+        return;
+    }
+
     moviesHolder.innerHTML = "";
 
+/*
     for (let movie of data.Search) {
         let movieCard = document.createElement("div");
-        movieCard.className = "card col-12 col-md-4 col-lg-3";
+        movieCard.className = "card col-12 col-md-4 col-lg-3 p-0";
 
         let movieImage = document.createElement("img");
+        movieImage.style.height = "500px";
+        movieImage.style.objectFit = "cover";
         movieImage.src = movie.Poster;
         movieImage.className = "card-img-top";
         movieImage.alt = movie.Title;
 
         let cardBody = document.createElement("div");
         cardBody.className = "card-body";
+        cardBody.className = "card-body bg-warning d-flex flex-column";
 
         let movieTitle = document.createElement("h5");
         movieTitle.className = "card-title";
         movieTitle.innerText = movie.Title;
 
         let movieYear = document.createElement("p");
-        movieYear.className = "card-text";
+        movieYear.className = "card-text mt-auto";
         movieYear.innerText = "Year: " + movie.Year;
 
         cardBody.append(movieTitle, movieYear);
@@ -38,5 +51,26 @@ document.getElementById("movieSearchForm").addEventListener("submit", async func
         movieCard.append(movieImage, cardBody);
 
         moviesHolder.append(movieCard);
+    }
+ */
+
+    for (let movie of data.Search) {
+        moviesHolder.innerHTML += `
+        <div class="card col-12 col-md-4 col-lg-3 p-0">
+            
+            <img 
+                src="${movie.Poster}" 
+                class="card-img-top" 
+                alt="${movie.Title}"
+                style="height: 500px; object-fit: cover;"
+            >
+
+            <div class="card-body bg-warning d-flex flex-column">
+                <h5 class="card-title">${movie.Title}</h5>
+                <p class="card-text mt-auto">Year: ${movie.Year}</p>
+            </div>
+
+        </div>
+    `;
     }
 });
