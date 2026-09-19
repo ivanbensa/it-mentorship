282177e42b63e46e3a1d54a2b0066955dd9f5670
ivanbensa <ivanbensa@proton.me>
2026-09-19 23:20:13 +0200

    Add exercise 2

--- JavaScript-exercises/10-dom-events-practice-2/script.js
+++ JavaScript-exercises/10-dom-events-practice-2/script.js
@@ -8,3 +8,15 @@ paragraph.innerText = "Welcome!";
 paragraph.style.fontSize = "18px";
 
 main.append(paragraph);
+
+const main2 = document.getElementById("main2");
+
+const names = ["Petar", "Luka", "Maja"];
+
+for (let name of names) {
+    if (name.includes("a")) {
+        const paragraph = document.createElement("p");
+        paragraph.innerText = name;
+        main2.append(paragraph);
+    }
+}
