bffc47d450e88725fed6762ab4c5a690ac87256d
ivanbensa <ivanbensa@proton.me>
2026-09-20 21:28:48 +0200

    Add exercises 3 and 4

--- JavaScript-exercises/10-dom-events-practice-2/script.js
+++ JavaScript-exercises/10-dom-events-practice-2/script.js
@@ -9,6 +9,8 @@ paragraph.style.fontSize = "18px";
 
 main.append(paragraph);
 
+//////////////////////////////   02   //////////////////////////////
+
 const main2 = document.getElementById("main2");
 
 const names = ["Petar", "Luka", "Maja"];
@@ -18,5 +20,30 @@ for (let name of names) {
         const paragraph = document.createElement("p");
         paragraph.innerText = name;
         main2.append(paragraph);
+    }
+}
+
+//////////////////////////////   03   //////////////////////////////
+
+const main3 = document.getElementById("main3");
+const score = 88;
+const paragraph3 = document.createElement("p");
+
+paragraph3.innerText = score >= 50 ? "Pass" : "Fail";
+
+main3.append(paragraph3);
+
+//////////////////////////////   04   //////////////////////////////
+
+const main4 = document.getElementById("main4");
+
+const numbers = [3, 6, 2, 9, 12, 15];
+
+for (let number of numbers) {
+    if (number % 3 === 0) {
+        const listItem = document.createElement("li");
+        listItem.innerText = `Number ${number} is divisible by 3`;
+
+        main4.append(listItem);
     }
 }
